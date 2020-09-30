d6b1492318603c309dfe975a934a3fc236923592
410501052 <71751364+410501052@users.noreply.github.com>
2020-09-30 15:33:48 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(80, 84))
+    basic.showString("A-")
+    basic.showNumber(randint(85, 89))
+    basic.showString("A")
+    basic.showNumber(randint(90, 100))
+    basic.showString("A+")
+})
+basic.forever(function () {
+	
+})
