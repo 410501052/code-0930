88dc7369cd6286e6bd1e7461b777f84c82d5153d
410501052 <71751364+410501052@users.noreply.github.com>
2020-09-30 15:40:35 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
+let GPA = 0
 input.onGesture(Gesture.Shake, function () {
+    let A3 = 0
+    let A2 = 0
+    let A = 0
+    GPA = A
     basic.showNumber(randint(80, 84))
     basic.showString("A-")
+    GPA = A2
     basic.showNumber(randint(85, 89))
     basic.showString("A")
+    GPA = A3
     basic.showNumber(randint(90, 100))
     basic.showString("A+")
 })
