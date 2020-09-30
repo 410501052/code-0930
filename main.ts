4d0a919fc9292d69630a6015a04403d33e68ccd1
410501052 <71751364+410501052@users.noreply.github.com>
2020-09-30 15:57:07 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
 let GPA = 0
 input.onGesture(Gesture.Shake, function () {
-    let A3 = 0
-    let A2 = 0
-    let A = 0
-    GPA = A
-    basic.showNumber(randint(80, 84))
-    basic.showString("A-")
-    GPA = A2
-    basic.showNumber(randint(85, 89))
-    basic.showString("A")
-    GPA = A3
-    basic.showNumber(randint(90, 100))
-    basic.showString("A+")
+    GPA = randint(80, 100)
+    basic.showNumber(GPA)
+    if (GPA >= 90) {
+        basic.showString("A+")
+    } else if (GPA >= 85) {
+        basic.showString("A")
+    } else if (GPA >= 80) {
+        basic.showString("A-")
+    }
 })
 basic.forever(function () {
 	
